28420bf09c2cf670a4c2b9166046a6d772214a47
reg <reg@bibi.(none)>
2010-02-12 11:33:31 +0300

    updated docstrings

--- patchs/usbvbox-backend/startup_localini.py
+++ patchs/usbvbox-backend/startup_localini.py
@@ -2,6 +2,24 @@
 # vim: ai ts=4 sts=4 et sw=4 coding=utf-8
 # maintainer: rgaudin
 
+''' USB VBOX backend helper
+
+    rewrites local.ini with correct UUID for USB modem
+    get from ssh connecting to host.
+
+    Edit following variables to set Vendor and Product Id
+    of the USB modem.
+
+    Adds the following bash code to /etc/rc.local
+
+    # execute rapidsms usb/vbox helper
+    # requires:
+    #    - 'host' in /etc/hosts to host' IP
+    #    - root's pubkey in chilcount@host ssh auth
+    /home/mvp/install/patchs/usbvbox-backend/startup_localini.py
+
+    logs as `usbvbox` on syslog daemon '''
+
 import subprocess
 import re
 import logging
@@ -22,12 +40,14 @@ def sanitize_hex(hex_):
 
 
 def get_uuid():
+    ''' calls VBoxManage on host and parse response to return UUID '''
 
     # read usb list from host
     output = subprocess.Popen(['/usr/bin/ssh', 'childcount@host', \
                                '/usr/bin/VBoxManage list usbhost'], \
                                stdout=subprocess.PIPE).communicate()[0]
 
+    # sanitize hex identifiers
     target_vendor = sanitize_hex(VENDOR_ID)
     target_product = sanitize_hex(PRODUCT_ID)
 
@@ -60,6 +80,7 @@ def get_uuid():
 
 
 def edit_localini(uuid):
+    ''' read local.ini, change uuid and rewrites it '''
 
     # read source line by line to find UUID one
     # store each file into output
@@ -81,19 +102,20 @@ def edit_localini(uuid):
 
 def main():
 
+    # initialize logging
     name = 'usbvbox'
     logger = logging.getLogger('')
     logger.setLevel(logging.DEBUG)
     logger.addHandler(logging.handlers.SysLogHandler(address='/dev/log', \
                                                      facility='daemon'))
-
-
+    # retrieve UUID
     uuid = get_uuid()
     if uuid == None:
         logging.error("%s: Can't find your device. Script configured ?" % name)
         exit(1)
     else:
         logging.error("%s: Found USB UUID: %s" % (name, uuid))
+    # UUID is found, edit local.ini
     edit_localini(uuid)
     logging.info("%s: Edited local.ini file (%s)" % (name, LOCALINI))
 
