6b80b9ccd5356712361074665569c88c0c5ac3a3
Henry C-G <noemail@noemail>
2011-07-13 08:45:56 +0300

    New ruhiira configs

--- childcount-2.0/ruhiira/home/django_wsgi.py
+++ childcount-2.0/ruhiira/home/django_wsgi.py
@@ -3,48 +3,45 @@
 # maintainer: rgaudin
 
 import os
-import sys
-import glob
-import time
-
 import django.core.handlers.wsgi
 
 filedir = os.path.dirname(__file__)  # this is in the rapidsms directory
 rootpath = "/home/childcount/sms"
 
+# set rapidsms home
+os.environ["RAPIDSMS_HOME"] = rootpath
+
 # load rapidsms config
 os.environ['RAPIDSMS_INI'] = os.path.join(rootpath, 'local.ini')
 from rapidsms.webui import settings
 
+# NOTE!!!!!!!
+# Save yourself hours of debugging
+# and make sure that whenever you
+# change the following celery settings
+# you change them in settings.py also
+settings.BROKER_HOST = "localhost"
+settings.BROKER_PORT = 5672
+settings.BROKER_USER = "childcount"
+settings.BROKER_PASSWORD = "childcount"
+settings.BROKER_VHOST = "ccvhost"
+
+settings.CELERY_RESULT_BACKEND = 'amqp'
+settings.CELERY_DISABLE_RATE_LIMITS = True
+settings.CELERY_LOADER='django'
+settings.CELERY_AMQP_TASK_RESULT_EXPIRES = 60*60
+settings.CELERY_IMPORTS = ('reportgen.definitions',)
+
 # set rapidsms config as django config
 os.environ['DJANGO_SETTINGS_MODULE'] = 'rapidsms.webui.settings'
 
-# set rapidsms home
-os.environ["RAPIDSMS_HOME"] = rootpath
-
-# Celery
-os.environ['CELERY_LOADER'] = 'django'
-
-# Reportlab fonts
-os.environ['RL_TTFSearchPath'] = '/home/childcount/sms/lib/ccdoc'
-
-# Get all dist packages
-sys.path.extend(glob.glob('/usr/local/lib/python2.6/dist-packages/*'))
-sys.path.extend('/home/childcount/sms/lib')
-
 # add non-rapidsms django apps
 from django.conf import settings
-settings.INSTALLED_APPS.extend(['reversion'])
-
-# add sms/lib to path
-sys.path.extend([os.path.join(rootpath, 'lib')])
-sys.path.extend([os.path.join(rootpath, 'ccdoc')])
+settings.INSTALLED_APPS.extend(['reversion','djcelery'])
 
 # create WSGI handler
 application = django.core.handlers.wsgi.WSGIHandler()
 
-# Set timezone
+import time
 os.environ['TZ'] = 'Africa/Kampala'
 time.tzset()
-
-
